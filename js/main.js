4c7b82c186cc7b2fc4c4fdce7569e3f7d89a34d0
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-04-28 16:18:36 -0500

    Implemented Smooth Scrolling and Copyright Symbols

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//smooth anchor scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
